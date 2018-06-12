ec2aff1e9fd4924b2a87944cfcecb10271e370e5
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-12 15:14:35 -0400

    Add messages to chat state

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MessageForm = () => {
     return (
         <form className="MessageForm">
-            <input type="text" name="body" placehodler="Type a message..." />
+            <input type="text" name="body" placeholder="Type a message..." />
             <button type="button">Send</button>
         </form>
     )
