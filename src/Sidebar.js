cd5c16113f2d8617553c411e745333e0fd07b94f
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-13 16:19:55 -0400

    added all of classwork files

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,68 +1,47 @@
-import React from 'react';
+import React from 'react'
 
-import UserInfo from './UserInfo';
-import RoomList from './RoomList';
+import UserInfo from './UserInfo'
+import RoomList from './RoomList'
 
-const Sidebar = (props) =>{
-    
-        return (
-            <aside className="Sidebar" style={styles.sidebar}>
-                <UserInfo user={props.user} />
-                <h1 style= {{
-                    ...styles.children,
-                    ...styles.h1,
-                }}>XTBC 18</h1>
-                <RoomList />
-          </aside>
-        )
+const Sidebar = ({ user, signOut }) => {
+  return (
+    <aside
+      className="Sidebar"
+      style={styles.sidebar}
+    >
+      <UserInfo user={user} signOut={signOut} />
+      <h1
+        style={{
+          ...styles.children,
+          ...styles.h1,
+        }}
+      >
+        XTBC 18
+      </h1>
+      <RoomList />
+    </aside>
+  )
 }
+
 const styles = {
-    sidebar: {
-        backgroundColor: '#333344',
-        color: 'rgba(255, 255, 255, 0.8)',
-        width: '12rem',
-        padding: '1rem 0',
-        display: 'flex',
-        flexDirection: 'column',
-        
-    },
+  sidebar: {
+    backgroundColor: '#333344',
+    color: 'rgba(255, 255, 255, 0.8)',
+    width: '12rem',
+    padding: '1rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+  },
 
-    children: {
-        padding: '0 1rem',
+  children: {
+    padding: '0 1rem',
+  },
 
-    },
-
-    h1: {
-        color:'white',
-        fontSize: '1.2rem',
-        marginTop: 0,
-
-    },
-    ///////////////////////////////////
-    h2: { //.RoomList 
-        fontSize: '1rem',
-      },
-      
-      ul: {
-        listStyle: 'none',
-        marginLeft: 0,
-        paddingLeft: 0,
-      },
-      
-      li: {
-        marginBottom: '0.5rem',
-      },
-      a: { //.RoomList li
-        display:'block',
-        color: 'whitesmoke',
-        textDecoration: 'none',
-      },
-    //   .RoomList li a::before {
-    //     content: '# ';
-    //   }
-    //   .RoomList li a:hover {
-    //     background-color: rgba(255, 255, 255, 0.2);
-    //   }
+  h1: {
+    color: 'white',
+    fontSize: '1.2rem',
+    marginTop: 0,
+  },
 }
 
-export default Sidebar;
+export default Sidebar
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React from 'react'
 
-import Avatar from './Avatar';
-import SignOut from './SignOut';
+import Avatar from './Avatar'
+import SignOut from './SignOut'
 
-const UserInfo = ({user}) =>{
-    return (
-        <div className="UserInfo" 
-            style={styles.info}>
-            <Avatar user={user} style={styles.Avatar} />
-               
-                    <div className="user" style={styles.user}>{user.userName}</div>
-                    <SignOut />
-                    {/* <a href="#">
-                        <i className="fas fa-sign-out-alt"></i>
-                    </a> */}
-        </div>
-    )
-
+const UserInfo = ({ user, signOut }) => {
+  return (
+    <div
+      className="UserInfo"
+      style={styles.info}
+    >
+      <Avatar user={user} style={styles.avatar} />
+      <div className="user" style={styles.user}>
+        {user.userName}
+      </div>
+      <SignOut signOut={signOut} />
+    </div>
+  )
 }
+
 const styles = {
-      info: {
-          marginBottom: '1rem',
-          display: 'flex',
-          alignItems: 'center',
-      },
-      Avatar: {
-        marginRight: '0.5rem',
-      },
-      user: {
-        flex: 1,
-      },
-      
+  info: {
+    marginBottom: '1rem',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 1rem',
+  },
+
+  avatar: {
+    marginRight: '0.5rem',
+  },
+
+  user: {
+    flex: 1,
+  },
 }
 
-export default UserInfo;
+export default UserInfo
