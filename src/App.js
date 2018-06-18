d48b994ed67b88e4239bad4d4f4aed1b79aaaa1a
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-18 16:09:55 -0400

    end of classwork additions for today

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
-import './App.css'
+import './App.css' 
 import { auth } from './Base'
 import SignIn from './SignIn'
 import Main from './Main'
